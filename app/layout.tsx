d05ac9e75e28effd89b400105c6e9d3aad1130fa
rainleee <rainlee4401@gmail.com>
2024-05-29 15:28:06 +0900

    feat: add msw settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import { ApolloWrapper } from "./components/apollo-wrapper";
+import GNB from "./components/gnb";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ApolloWrapper>
+          <GNB>{children}</GNB>
+        </ApolloWrapper>
+      </body>
     </html>
   );
 }
